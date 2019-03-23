3a0c4b344749a816dd418c43ccb51443e5cb6dbf
Brian Tobal <brian.tobal@gmail.com>
2019-03-23 14:18:25 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,21 +14,13 @@ require 'pry'
   #   }
   # }
 
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice_cream| ice_cream == "strawberry"}
-      end
-    end
-  end
-end
+
   
 #def remove_strawberry(contacts)
 #  contacts.each do |person, details|
 #    details.each do |attribute, data|
 #     if attribute == :favorite_ice_cream_flavors
-#      data.delete_if == {|flavor| flavor == "strawberry"}
+#      data.delete_if  {|flavor| flavor == "strawberry"}
 #     end 
 #    end 
 #  end  
